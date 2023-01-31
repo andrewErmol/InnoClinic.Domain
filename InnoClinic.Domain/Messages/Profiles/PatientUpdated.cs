272b054e4e7ebc9683e5e83d7f03f5d3a31e4562
andrewErmol <andrew0erm2@gmail.com>
2023-01-31 20:18:36 +0300

    Add Profiles and serviceMessages

---
+++ InnoClinic.Domain/Messages/Profiles/PatientUpdated.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoClinic.Domain.Messages.Profiles
+{
+    public class PatientUpdated
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ InnoClinic.Domain/Messages/Service/ServiceUpdated.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoClinic.Domain.Messages.Service
+{
+    public class ServiceUpdated
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
